eb70fb7baeec1db1b3aa994216e8c6808058c31f
alameen.kh <alameen.kh@charmboard.com>
2020-05-24 11:16:04 +0530

    babel config files are added

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import {render} from 'react-dom';
+import React, {Component} from 'react';
+
+class PokeApp extends Component{
+  render(){
+    return <div className="pokeapp">
+      <h1> The Kanto PokeDex! </h1>
+    </div>;
+  }
+}
+render(<PokeApp/>,document.getElementById('app'))
