8e2a24bc28cb8fd494bbcd89f3b7ca3eb77821ab
alameen.kh <alameen.kh@charmboard.com>
2020-05-24 11:24:46 +0530

    List Pokemon feature added

---
+++ README.md
@@ -0,0 +1,9 @@
+##
+
+first install node modules
+
+npm install
+
+run the dev server with the command
+
+node_modules/webpack-dev-server/bin/webpack-dev-server.js
--- src/index.js
+++ src/index.js
@@ -1,11 +1,78 @@
 import {render} from 'react-dom';
 import React, {Component} from 'react';
+import fetch from 'isomorphic-fetch';
 
-class PokeApp extends Component{
+//this class is the main js file
+//extend the reactor component to 
+//have component structure
+
+
+class Pokemon extends Component{
   render(){
-    return <div className="pokeapp">
-      <h1> The Kanto PokeDex! </h1>
+    const {pokemon,id} = this.props;
+    return <div className="pokemon--species">
+            <div className="pokemon--species--container">
+              <div className="pokemon--species--sprite">
+                <img src={`/public/sprites/${id}.png`} />
+              </div>
+              <div className="pokemon--species--name"> {pokemon.name} </div>
+            </div>
+          </div>;
+    }
+}
+
+
+
+
+class PokemonList extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      species : [],
+      fetched : false,
+      loading : false,
+    };
+  }
+  componentWillMount(){
+    this.setState({
+      loading : true
+    });
+    fetch('http://pokeapi.co/api/v2/pokemon?limit=151').then(res=>res.json())
+    .then(response=>{
+      this.setState({
+        species : response.results,
+        loading : true,
+        fetched : true
+      });
+    });
+  }
+
+  render(){
+    const {fetched, loading, species} = this.state;
+    let content ;
+    if(fetched){
+      content = <div className="pokemon--species--list">{species.map((pokemon,index)=><Pokemon key={pokemon.name} id={index+1} pokemon={pokemon}/>)}</div>;
+    }else if(loading && !fetched){
+        content = <p> Loading ...</p>;
+    }
+    else{
+      content = <div/>;
+    }
+    return  <div>
+      {content}
     </div>;
   }
 }
+
+
+//This is the root component
+class PokeApp extends Component{
+  render(){
+    return <div className="pokeapp">
+      <h1> Pokedex List</h1>
+      <PokemonList/>
+    </div>;
+  }
+}
+
 render(<PokeApp/>,document.getElementById('app'))
